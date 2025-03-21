ff5171798ae0eb769aa56b6402965f9928cc7f99
Petru-Sebastian Toader <mail@petrutoader.com>
2025-03-22 00:07:14 +0200

    Fix trademark and copyright notice

--- main_zh.ts
+++ main_zh.ts
@@ -40,7 +40,7 @@
     <message>
         <location filename="../src/AboutWindow.cpp" line="187"/>
         <source>™ and © 2020-2024 Epilogue. &lt;br&gt;All rights reserved. </source>
-        <translation>and © 2020-2024 Epilogue.&lt;br&gt;版权所有。 </translation>
+        <translation>™ and © 2020-2024 Epilogue.&lt;br&gt;版权所有。 </translation>
     </message>
     <message>
         <location filename="../src/AboutWindow.cpp" line="178"/>
